58a1eb6a67b5a622240ce4b1f530c4d2a3a372c2
mlazoy <126875749+mlazoy@users.noreply.github.com>
2023-12-09 00:09:48 +0200

    Delete SL directory

--- SL/Project/main.py
+++
@@ -1,6 +0,0 @@
-from website import create_app
-
-app = create_app()
-
-if __name__ == '__main__': # needed so that the app does not run when imported
-    app.run(debug=True, host='localhost', port=5000) # debug needed for reruning the server when we make code changes
